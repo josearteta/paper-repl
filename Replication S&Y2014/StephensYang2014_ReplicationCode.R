9cafb8d02458136bff71df9f822220bf24807236
Igor Brito <54274767+ABritoIgor@users.noreply.github.com>
2023-04-26 11:44:29 -0300

    Added columns I to III from table I

--- Replication S&Y2014/StephensYang2014_ReplicationCode.R
+++ Replication S&Y2014/StephensYang2014_ReplicationCode.R
@@ -31,9 +31,13 @@ library(fastDummies)
 #-----------------------------------------------
 
 census <- readRDS("census.rds")
+# For columns I and II
 census_40to49 <- readRDS("census_40to49.rds")
-census_whites <- readRDS("census_whites.rds")
+# For columns III and IV
 census_whites_male <- readRDS("census_whites_male.rds")
+# For columns V and VI
+census_whites <- readRDS("census_whites.rds")
+# For columns VII and VIII
 census_whites_non_south <- readRDS("census_whites_non_south.rds")
 census_whites_south <- readRDS("census_whites_south.rds")
 
@@ -65,13 +69,18 @@ interactions_complete <- c(colnames(census)[148:294])
 interactions_south_40to49 <- c(colnames(census)[153:181])
 interactions_west_40to49 <- c(colnames(census)[202:230])
 interactions_midwest_40to49 <- c(colnames(census)[251:279])
+interactions_40to49 <- c(interactions_midwest_40to49,interactions_south_40to49,interactions_west_40to49)
+additional_controls <- c("age","age2","age3","age4","census70","census80")
+
+#---------------------------
+# TABLE I
+#---------------------------
 
 
-# Table I
 
-# Need to try FWL to see if it is significantly more efficient
-
-## Column 1
+#-------------
+# COMLUMN I
+#-------------
 
 ### OLS Estimation for the first column
 col1_ols <- lm(reformulate(c("gradcap",state_fe,yob_fe_40to49),
@@ -82,82 +91,132 @@ coeftest(col1_ols,vcovCL,cluster = census_40to49$yob_bplg)
 
 
 ### IV Regression for the first column
-col1_1st_stage <- lm(gradcap ~ ny7 + ny8 + ny9 + factor(yob) + factor(bplg),
+col1_1st_stage <- lm(reformulate(c(instruments,state_fe,yob_fe_40to49),
+                                 response = "gradcap"),
                           weights = slwt,
                           data = census_40to49)
 coeftest(col1_1st_stage,vcovCL,cluster = census_40to49$yob_bplg)
 grad_hat <- col1_1st_stage$fitted.values # Fitted values 
 
-### F statistic cluster robust
-col1_1st_stage_F <- lm(gradcap ~ factor(yob) + factor(bplg),
-                       weights = slwt,
-                       data = census_40to49)
-
-linearHypothesis(col1_1st_stage,instruments)
 
 ### Second stage of the first column
-col1_2nd_stage <- lm(lnwkwage ~ col1_1st_stage_test$fitted.values + factor(yob) + factor(bplg),
-                          weights = slwt,
-                          data = census_40to49)
-coeftest(col1_2nd_stage,vcovCL,cluster = census_40to49$yob_bplg)[2,]
-summary(col1_2nd_stage_test)
 
+### Using FWL for the IV estimation
+out_adj <- lm(reformulate(c(state_fe,yob_fe_40to49),
+                          response = "lnwkwage"),
+              weights = slwt,
+              data = census_40to49)$residuals
 
-## Column 2 
+grad_adj <- lm(reformulate(c(state_fe,yob_fe_40to49),
+                          response = "gradcap"),
+              weights = slwt,
+              data = census_40to49)$residuals
+
+ny7_adj <- lm(reformulate(c(state_fe,yob_fe_40to49),
+                          response = "ny7"),
+              weights = slwt,
+              data = census_40to49)$residuals
+ny8_adj <- lm(reformulate(c(state_fe,yob_fe_40to49),
+                          response = "ny8"),
+              weights = slwt,
+              data = census_40to49)$residuals
+ny9_adj <- lm(reformulate(c(state_fe,yob_fe_40to49),
+                          response = "ny9"),
+              weights = slwt,
+              data = census_40to49)$residuals
+col1_iv <- ivreg(out_adj ~ grad_adj | ny7_adj + ny8_adj + ny9_adj,
+                 weights = slwt,
+                 data = census_40to49)
+summary(col1_iv)
+
+#-------------
+# COMLUMN II
+#-------------
 
 ### OLS regression for column 2 
-col2_ols <- lm(lnwkwage ~ gradcap + factor(yob):southern_born + 
-                 factor(yob):midwestern_born + factor(yob):western_born  + factor(bplg) + factor(yob),
+col2_ols <- lm(reformulate(c("gradcap",state_fe,yob_fe_40to49,interactions_40to49),
+                           response = "lnwkwage"),
                weights = slwt,
                data = census_40to49)
 coeftest(col2_ols,vcovCL,cluster = census_40to49$yob_bplg)
 
 ### IV regression for column 2
 #### First stage
-col2_1st_stage <- lm(reformulate(c(instruments,state_fe,yob_fe_40to49,interactions_midwest_40to49,
-                                   interactions_south_40to49,interactions_west_40to49),
+col2_1st_stage <- lm(reformulate(c(instruments,state_fe,yob_fe_40to49,interactions_40to49),
                                  response = "gradcap"),
                      weights = slwt,
                      data = census_40to49)
 summary(col2_1st_stage)
 
-## Column 3
+
+#### Using FWL to calculate the IV estimation
+out_adj <- lm(reformulate(c(state_fe,yob_fe_40to49,interactions_40to49),
+                          response = "lnwkwage"),
+              weights = slwt,
+              data = census_40to49)$residuals
+
+grad_adj <- lm(reformulate(c(state_fe,yob_fe_40to49,interactions_40to49),
+                           response = "gradcap"),
+               weights = slwt,
+               data = census_40to49)$residuals
+
+ny7_adj <- lm(reformulate(c(state_fe,yob_fe_40to49,interactions_40to49),
+                          response = "ny7"),
+              weights = slwt,
+              data = census_40to49)$residuals
+ny8_adj <- lm(reformulate(c(state_fe,yob_fe_40to49,interactions_40to49),
+                          response = "ny8"),
+              weights = slwt,
+              data = census_40to49)$residuals
+ny9_adj <- lm(reformulate(c(state_fe,yob_fe_40to49,interactions_40to49),
+                          response = "ny9"),
+              weights = slwt,
+              data = census_40to49)$residuals
+col2_iv <- ivreg(out_adj ~ grad_adj | ny7_adj + ny8_adj + ny9_adj,
+                 weights = slwt,
+                 data = census_40to49)
+summary(col2_iv)
+
+
+#-------------
+# COMLUMN III
+#-------------
 
 ### OLS
-col3_ols <- lm(reformulate(c("gradcap","age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+col3_ols <- lm(reformulate(c("gradcap",additional_controls,state_fe,yob_fe),
                            response = "lnwkwage"),
                weights = slwt,
                data = census_whites_male)
+summary(col3_ols)
 ### 1st Stage 
-col3_1st_stage <- lm(reformulate(c(instruments,"age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+col3_1st_stage <- lm(reformulate(c(instruments,additional_controls,state_fe,yob_fe),
                                  response = "gradcap"),
                      weights = slwt,
                      data = census_whites_male)
 summary(col3_1st_stage)
 
 ### 2SLS Using FWL
-
-out_adj <- lm(reformulate(c("age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+out_adj <- lm(reformulate(c(additional_controls,state_fe,yob_fe),
                           response = "lnwkwage"),
               weights = slwt,
               data = census_whites_male)$residuals
 
-grad_adj <- lm(reformulate(c("age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+grad_adj <- lm(reformulate(c(additional_controls,state_fe,yob_fe),
                           response = "gradcap"),
               weights = slwt,
               data = census_whites_male)$residuals
 
-ny7_adj <- lm(reformulate(c("age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+ny7_adj <- lm(reformulate(c(additional_controls,state_fe,yob_fe),
                           response = "ny7"),
               weights = slwt,
               data = census_whites_male)$residuals
 
-ny8_adj <- lm(reformulate(c("age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+ny8_adj <- lm(reformulate(c(additional_controls,state_fe,yob_fe),
                           response = "ny8"),
               weights = slwt,
               data = census_whites_male)$residuals
 
-ny9_adj <- lm(reformulate(c("age","age2","age3","age4","census70","census80",state_fe,yob_fe),
+ny9_adj <- lm(reformulate(c(additional_controls,state_fe,yob_fe),
                           response = "ny9"),
               weights = slwt,
               data = census_whites_male)$residuals
@@ -167,7 +226,11 @@ col3_iv <- ivreg(out_adj ~ grad_adj | ny7_adj + ny8_adj + ny9_adj,
                  data = census_whites_male)
 summary(col3_iv)
 
-## Column 4
+
+#-------------
+# COMLUMN IV
+#-------------
+
 
 ### OLS
 
